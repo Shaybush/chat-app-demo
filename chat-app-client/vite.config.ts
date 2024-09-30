995d048635e5a88cf85f6fac4878137b1291ad1c
Shay Bushary <shaybush93@gmail.com>
2024-09-30 16:16:29 +0300

    docker setup completed

--- chat-app-client/vite.config.ts
+++ chat-app-client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: true, // This makes the server listen on all network interfaces
+		port: 5173,
+	},
 });
