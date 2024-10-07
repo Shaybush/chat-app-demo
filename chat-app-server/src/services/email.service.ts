c1f94b1ffa299f0bbe36389d32e1708db086ce21
0B-111 <bitanomri@gmail.com>
2024-10-07 17:32:05 +0300

    create set service under redis

--- chat-app-server/src/services/email.service.ts
+++ chat-app-server/src/services/email.service.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import emailService from '../utils/email.util'
-import { get_set_redis_cache } from "../utils/redis.util";
+import { set_redis_cache } from "../utils/redis.util";
 import { create_email_code, generate_verification_email, generateTime } from "../utils/function.util";
+import bcrypt from 'bcrypt'
 
 const DIGITS_LEN = 6;
 
@@ -9,11 +10,12 @@ export const send_digit_to_email = async (req: Request, res: Response, next: Nex
 	// TODO - add controller here 
 	const { email } = req.body;
 	const digits = create_email_code(DIGITS_LEN)
+	const hashedPassword = await bcrypt.hash(JSON.stringify(digits),10);	
 	try {
-		await get_set_redis_cache({
+		await set_redis_cache({
 			key: email,
 			expirationTime: generateTime({ value: 1, time: 'hour' }),
-			callbackFn: () => digits
+			callbackFn: () => hashedPassword
 		})	
 		await  emailService.sendEmail(generate_verification_email(email, digits));
 	} catch (error) {
