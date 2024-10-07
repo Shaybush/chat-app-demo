f66967dfdd8c4401e2b608c873d225493f3725d5
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2024-10-07 16:06:03 +0300

    Feature (#4)
    * docker setup completed
    
    * change docker file to run pnpm
    
    * docker-compose ready and works
    
    * change to Promise.allSettled

---
+++ chat-app-server/src/utils/redis.util.ts
@@ -0,0 +1,29 @@
+import { RedisClientType } from 'redis';
+import connectRedis from '../db/connect-redis';
+import { AppError } from '../error/appError';
+import CommonResponseDict from './common-response-dict.utils';
+
+interface IRedisCacheProps {
+    key: string;
+    callbackFn: () => void;
+    expirationTime: number
+}
+
+export const get_set_redis_cache = async ({ key, callbackFn, expirationTime }: IRedisCacheProps): Promise<void | RedisClientType> => {
+    const redisClient = await connectRedis()
+    return new Promise((resolve, reject) => {
+        redisClient.GET(key)
+            .then((redisData: string) => {
+                if (redisData && redisData !== null) return resolve(JSON.parse(redisData));
+                const value = callbackFn();
+                redisClient.SETEX(key, expirationTime, JSON.stringify(value));
+                resolve(value);
+            })
+            .catch((error: { message: string }) => reject(new AppError(
+                CommonResponseDict.InternalServerError.title,
+                CommonResponseDict.InternalServerError.code,
+                `error getting redis data: ${error.message}`,
+                false
+            )))
+    })
+}
